8a8dee1ba09e383692249f701d8cd94ebffbacf6
Vernon Sung <vernonsung@itri.org.tw>
2015-09-11 21:38:00 +0800

    TODO: finish camera button.

--- Application/src/main/java/com/example/android/activityscenetransitionbasic/CreateItemActivity.java
+++ Application/src/main/java/com/example/android/activityscenetransitionbasic/CreateItemActivity.java
@@ -1,16 +1,30 @@
 package com.example.android.activityscenetransitionbasic;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class CreateItemActivity extends Activity {
 
+    // The request code to invoke a camera APP
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     private ImageView mImageView;
+    private ImageButton mButtonCamera;
+    private ImageButton mButtonGallery;
+    private Button mButtonMore;
+    private Button mButtonSend;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +36,18 @@ public class CreateItemActivity extends Activity {
                     .load("http://testgcsserver.appspot.com.storage.googleapis.com/image_frame.png")
                     .into(mImageView);
         }
+
+        mButtonCamera = (ImageButton) findViewById(R.id.imagebutton_camera);
+        mButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        mButtonGallery = (ImageButton) findViewById(R.id.imagebutton_gallery);
+        mButtonMore = (Button) findViewById(R.id.button_create_item_more);
+        mButtonSend = (Button) findViewById(R.id.button_create_item_send);
     }
 
     @Override
@@ -44,5 +70,22 @@ public class CreateItemActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+            // TODO:
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
